3b487f1b4f9ff88729f75923bdabfe12ba80faa0
Jamie Rainey <jamesrainey54@hotmail.co.uk>
2023-04-17 12:31:05 +0100

    Add Radio files

--- include/radio.hpp
+++ include/radio.hpp
@@ -6,10 +6,15 @@
 namespace bzzz
 {   
     /**
-     * 
+     * @brief Read the receiver data sent from Pi
      */
     void readPiData(void);
 
+    /**
+     * @brief reformat receiver values
+     */
+    void readRadioData(int &radioThrottle, int &radioRoll, int &radioPitch, int &radioYaw, int &radioSwitchC,
+    int &radioVRA, int &radioVRC, int &radioVRB, int &radioArm, int &radioKill, int &radioSwitchD, int &radioVRE);
 
 }
 
--- src/radio.cpp
+++ src/radio.cpp
@@ -2,10 +2,11 @@
 
 namespace bzzz
 {   
+    int data[16];
+
     void readPiData(void)
     {
         String dataFromClient;
-        int data[16];
         
         if (Serial.available() > 0) 
         {
@@ -22,5 +23,23 @@ namespace bzzz
         }
     }
 
+    void readRadioData(int &radioThrottle, int &radioRoll, int &radioPitch, int &radioYaw, int &radioSwitchC,
+    int &radioVRA, int &radioVRC, int &radioVRB, int &radioArm, int &radioKill, int &radioSwitchD, int &radioVRE) 
+    {
+        readPiData();
+        // reformat receiver values to match radio (RadioLink AT10)
+        radioThrottle = data[2];
+        radioRoll = data[3];
+        radioPitch = data[1];
+        radioYaw = data[0];
+        radioSwitchC = data[4];
+        radioVRA = data[5];
+        radioVRC = data[6];
+        radioVRB = data[7];
+        radioArm = data[8]; // radioSwitchB
+        radioKill = data[9]; // radioSwitchA
+        radioSwitchD = data[10];
+        radioVRE = data[11];
+    }
 
 }
