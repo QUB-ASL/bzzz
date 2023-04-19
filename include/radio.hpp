bd78787fe07546e5b71d73939565b8282a201d35
Jamie Rainey <jamesrainey54@hotmail.co.uk>
2023-04-19 15:10:00 +0100

    complete implementation of RC interface

--- include/radio.hpp
+++ include/radio.hpp
@@ -5,6 +5,16 @@
 
 namespace bzzz
 {   
+
+    /**
+     * Possible values of 3-way switch
+    */
+    enum ThreeWaySwitch {
+        DOWN = 0,
+        MID = 1,
+        UP = 2
+    };
+
     /**
      * @brief Read the receiver data sent from Pi
      */
@@ -13,8 +23,71 @@ namespace bzzz
     /**
      * @brief reformat receiver values
      */
-    void readRadioData(int &radioThrottle, int &radioRoll, int &radioPitch, int &radioYaw, int &radioSwitchC,
-    int &radioVRA, int &radioVRC, int &radioVRB, int &radioArm, int &radioKill, int &radioSwitchD, int &radioVRE);
+    void readRadioData(
+        int &radioThrottle, 
+        int &radioRoll, 
+        int &radioPitch, 
+        int &radioYawRate, 
+        int &radioSwitchC,
+        int &radioVRA, 
+        int &radioVRC, 
+        int &radioVRB, 
+        int &radioArm, 
+        int &radioKill, 
+        int &radioSwitchD, 
+        int &radioVRE);
+    
+    /**
+     * @brief pitch reference from RC in rad
+     */
+    float pitchReferenceAngleRad();
+    /**
+     * @brief roll reference from RC in rad
+     */
+    float rollReferenceAngleRad();
+    /**
+     * @brief throll reference as a percentage
+     */
+    float throttleReferencePercentage();
+    /**
+     * @brief yaw rate in rad/s 
+     * 
+     * The max yaw rate (abs) is 45deg/s
+     */
+    float yawRateReferenceRadSec();
+    /**
+     * @brief whether the arm switch is on
+     */
+    bool armed();
+    /**
+     * @brief whether the kill switch is on
+     */
+    bool kill();
+    /**
+     * @brief value of switch C (low/mid/high)
+     */
+    ThreeWaySwitch switchC();
+    /**
+     * @brief value of switch D
+     */
+    bool switchD();
+    /**
+     * @brief value of trimmer VRA
+     */
+    float trimmerVRAPercentage();
+    /**
+     * @brief value of trimmer VRB
+     */
+    float trimmerVRBPercentage();
+    /**
+     * @brief value of trimmer VRC
+     */
+    float trimmerVRCPercentage();
+    /**
+     * @brief value of trimmer VRE
+     */
+    float trimmerVREPercentage();
+
 
 }
 
--- src/radio.cpp
+++ src/radio.cpp
@@ -2,7 +2,22 @@
 
 namespace bzzz
 {   
-    int channelData[16]; // A 16 int array to store the 16 channels of data sent from the Pi (receiver) via serial 
+    /**
+     * Channel    Variable
+     * 1          yaw rate
+     * 2          pitch
+     * 3          throttle
+     * 4          roll
+     * 5          Switch C
+     * 6          Trimmer VRA 
+     * 7          Trimmer VRC
+     * 8          Trimmer VRB
+     * 9          Switch B (for arming)
+     * 10         Switch A (kill)
+     * 11         Switch D
+     * 12         Trimmer VRE
+     */
+    static int channelData[16];
 
     void readPiData(void)
     {
@@ -10,35 +25,111 @@ namespace bzzz
         
         if (Serial.available() > 0) 
         {
-            allDataFromPi = Serial.readStringUntil('\n'); //all 16 channels read by ESP32 
+            allDataFromPi = Serial.readStringUntil('\n');
 
             for (int i = 0; i < 16; i++) 
             {
-                // take the substring from the start to the first occurence of a comma, convert it to int and save it in the array
+                // take the  substring from the start to the first occurence of a comma, convert it to int and save it in the array
                 channelData[i] = allDataFromPi.substring(1, allDataFromPi.indexOf(",")).toInt();
-
-                //cut the data string after the first occurence of a comma
+    
+                //cut the channelData string after the first occurence of a comma
                 allDataFromPi = allDataFromPi.substring(allDataFromPi.indexOf(",") + 1);
             }
         }
     }
 
-    void readRadioData(int &radioThrottle, int &radioRoll, int &radioPitch, int &radioYaw, int &radioSwitchC,
-    int &radioVRA, int &radioVRC, int &radioVRB, int &radioArm, int &radioKill, int &radioSwitchD, int &radioVRE) 
+    void readRadioData(
+        int &radioThrottle, 
+        int &radioRoll, 
+        int &radioPitch, 
+        int &radioYawRate, 
+        int &radioSwitchC,
+        int &radioVRA, 
+        int &radioVRC, 
+        int &radioVRB, 
+        int &radioArm, 
+        int &radioKill, 
+        int &radioSwitchD, 
+        int &radioVRE)
     {
-        // reformat receiver values to match radio (RadioLink AT10)
+        // reformat receiver values  to match radio (RadioLink AT10)
         radioThrottle = channelData[2];
         radioRoll = channelData[3];
         radioPitch = channelData[1];
-        radioYaw = channelData[0];
+        radioYawRate = channelData[0];  
         radioSwitchC = channelData[4];
         radioVRA = channelData[5];
         radioVRC = channelData[6];
-        radioVRB = channelData[7];
-        radioArm = channelData[8]; // radioSwitchB
-        radioKill = channelData[9]; // radioSwitchA
+        radioVRB = channelData  [7];
+        radioArm = channelData[ 8]; // radioSwitchB
+        radioKill = channelData[9]  ; // radioSwitchA
         radioSwitchD = channelData[10];
         radioVRE = channelData[11];
+    }
+
+
+    static float mapRadioToAngle(float x){
+        return -PITCH_MAX_RAD + (x - RADIO_STICK_MIN)/(RADIO_STICK_MAX - RADIO_STICK_MIN)*2*PITCH_MAX_RAD;
+    }
+
+    float pitchReferenceAngleRad(){ 
+        return mapRadioToAngle(channelData[1]);
+    }
+
+    float rollReferenceAngleRad(){ 
+        return mapRadioToAngle(channelData[3]);
+    }
+
+    float yawRateReferenceRadSec() 
+    {
+        return -RADIO_MAX_YAW_RATE_RAD_SEC + 
+            (channelData[0] - RADIO_STICK_MIN)
+                /(RADIO_STICK_MAX - RADIO_STICK_MIN)*2*RADIO_MAX_YAW_RATE_RAD_SEC;
+    }
+
+    float throttleReferencePercentage() {
+        return  (channelData[2] - RADIO_STICK_MIN)/(RADIO_STICK_MAX - RADIO_STICK_MIN);
+    }
+
+    bool armed()
+    {
+        return channelData[8] >= 1500;
+    }
+
+    bool kill()
+    {
+        return channelData[9] <= 500;
+    }
+
+    ThreeWaySwitch switchC()
+    {
+        int switchValue = channelData[4];
+        if (switchValue <= 450) {
+            return ThreeWaySwitch::DOWN;
+        } else if (switchValue <= 1200) {
+            return ThreeWaySwitch::MID;
+        }
+        return ThreeWaySwitch::UP;
+    }
+
+    bool switchD(){
+        return channelData[4];
+    }
+
+    float trimmerVRAPercentage(){
+        return (channelData[5] - RADIO_STICK_MIN)/(RADIO_STICK_MAX - RADIO_STICK_MIN);
+    }
+
+    float trimmerVRCPercentage(){
+        return (channelData[6] - RADIO_STICK_MIN)/(RADIO_STICK_MAX - RADIO_STICK_MIN);
+    }
+
+    float trimmerVRBPercentage(){
+        return (channelData[7] - RADIO_STICK_MIN)/(RADIO_STICK_MAX - RADIO_STICK_MIN);
+    }
+
+    float trimmerVREPercentage(){
+        return (channelData[11] - RADIO_STICK_MIN)/(RADIO_STICK_MAX - RADIO_STICK_MIN);
     }
 
 }
