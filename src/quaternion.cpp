1a5da019d2fe2bc7365f1ced15f439f045be9e61
Pantelis Sopasakis <p.sopasakis@gmail.com>
2023-04-21 00:06:56 +0100

    fix quaternion difference
    About:
    fix wrong sign in quaternion difference
    
    Issues:
    closes #22

--- src/quaternion.cpp
+++ src/quaternion.cpp
@@ -42,7 +42,7 @@ namespace bzzz
     {
         Quaternion diff;
         diff[0] = lhs[0] * rhs[0] + lhs[1] * rhs[1] + lhs[2] * rhs[2] + lhs[3] * rhs[3];
-        diff[1] = lhs[0] * rhs[1] - lhs[1] * rhs[0] - lhs[2] * rhs[3] + lhs[3] * rhs[2];
+        diff[1] = -lhs[0] * rhs[1] + lhs[1] * rhs[0] - lhs[2] * rhs[3] + lhs[3] * rhs[2];
         diff[2] = -lhs[0] * rhs[2] + lhs[1] * rhs[3] + lhs[2] * rhs[0] - lhs[3] * rhs[1];
         diff[3] = -lhs[0] * rhs[3] - lhs[1] * rhs[2] + lhs[2] * rhs[1] + lhs[3] * rhs[0];
         return diff;
