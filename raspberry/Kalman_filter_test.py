35e4d248154abda697a452d61b9ac088461d0c9a
gunturiCM <gunturicm@gmail.com>
2023-06-15 00:19:02 +0100

    Made changes suggested in review 1.

---
+++ raspberry/Kalman_filter_test.py
@@ -0,0 +1,42 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+from simulations.altitude_dynamics import AltitudeDynamics
+from estimators.altitude_Kalman_filter import KalmanFilter
+
+
+num_data_points_collected = 100
+sampling_frequency = 40
+sampling_time = 1./sampling_frequency
+time_stamps = [i*sampling_time for i in range(num_data_points_collected)]
+
+
+# Simulate the dynamics and fake the measurement data for testing the least squares estimations
+dynamics = AltitudeDynamics(sampling_time=sampling_time)
+Tref_t = [20*np.sin(0.1*t) for t in range(num_data_points_collected)]
+for i in range(num_data_points_collected - 1):
+    dynamics.simulate(Tref_t=Tref_t[i])
+
+
+kf = KalmanFilter(initial_Tt=Tref_t[0], cache_values=True)
+
+y_t = np.zeros(num_data_points_collected)
+for i in range(num_data_points_collected):
+    y_t[i] = dynamics.z[i]
+    kf.run(Tref_t[i], y_t[i]).reshape(4, )
+
+
+x_hat_t, _ = kf.MU_cache()
+x_hat_t = np.array(x_hat_t).reshape((num_data_points_collected, 4))
+print("alpha hat = %f"%np.max(x_hat_t[:, 2]), "\nc hat = %f"%np.max(x_hat_t[:, 3]))
+
+fig, sub_plts = plt.subplots(2)
+
+sub_plts[0].plot(x_hat_t[:, 0])
+sub_plts[0].plot(y_t)
+sub_plts[0].legend([r"$\hat{x}_t$", r"$y_t$"])
+
+sub_plts[1].plot(x_hat_t[:, 2])
+sub_plts[1].plot(x_hat_t[:, 3])
+sub_plts[1].legend([r"$\hat{\alpha}_t$", r"$\hat{\beta}_t$"])
+plt.show()
--- raspberry/estimators/altitude_Kalman_filter.py
+++ raspberry/estimators/altitude_Kalman_filter.py
@@ -1,64 +1,59 @@
 # TODO: Documentation
 import numpy as np
-from matplotlib import pyplot as plt
 
 class KalmanFilter:
-    def __init__(self, sampling_frequency=10, x_tilde_0=np.array([[0,]*4]).T, P_0=np.array([[1, 1, 1, 1],]*4), cache_values=False) -> None:
+    def __init__(self, sampling_frequency=10, initial_Tt=0., x_tilde_0=np.zeros((4, 1)), P_0=np.eye(4, 4)*100, cache_values=False) -> None:
         self.__fs = sampling_frequency
         self.__Ts = 1/self.__fs
 
         # Normalized Throttle reference, range [0, 1]
-        self.__Tt = 0.
+        self.__Tt = initial_Tt
         # System dynamics
         self.__At = self.__update_At()
         self.__C = np.array([[1, 0, 0, 0]])
 
         # Process noise
-        self.__Q = np.array([
-            [1, 1, 1, 1],
-            [1, 1, 1, 1],
-            [1, 1, 1, 1],
-            [1, 1, 1, 1]
-        ])
+        self.__Q = np.eye(4, 4)*10**-5
         # Measurement noise
-        self.__R = 1
+        self.__R = 10**-3
 
         # initial conditions
         self.__x_hat_0_minus1 = x_tilde_0
         self.__sigma_0_minus1 = P_0
         
         # Measurement update
-        self.__x_hat_t_t = None
-        self.__sigma_t_t = None
+        self.__x_MU = None
+        self.__sigma_MU = None
 
         # Time update
-        self.__x_hat_t_plus1_t = x_tilde_0
-        self.__sigma_t_plus1_t = P_0
+        self.__x_TU = x_tilde_0
+        self.__sigma_TU = P_0
 
         self.__cache_MU_values = lambda : ()
         self.__cache_TU_values = lambda : ()
 
         if cache_values:
             # Measurement update cache
-            self.__x_hat_t_t_cache = []
-            self.__sigma_t_t_cache = []
+            self.__x_MU_cache = []
+            self.__sigma_MU_cache = []
 
             def cache_MU():
-                self.__x_hat_t_t_cache.append(self.__x_hat_t_t)
-                self.__sigma_t_t_cache.append(self.__sigma_t_t)
+                self.__x_MU_cache.append(self.__x_MU)
+                self.__sigma_MU_cache.append(self.__sigma_MU)
 
             self.__cache_MU_values = cache_MU
 
             # Time update cache
-            self.__x_hat_t_plus1_t_cache = []
-            self.__sigma_t_plus1_t_cache = []
+            self.__x_TU_cache = []
+            self.__sigma_TU_cache = []
 
             def cache_TU():
-                self.__x_hat_t_plus1_t_cache.append(self.__x_hat_t_plus1_t)
-                self.__sigma_t_plus1_t_cache.append(self.__sigma_t_plus1_t)
+                self.__x_TU_cache.append(self.__x_TU)
+                self.__sigma_TU_cache.append(self.__sigma_TU)
 
             self.__cache_TU_values = cache_TU
-
+            # cache first MU from above
+            self.__cache_TU_values()
 
     def __update_At(self):
         self.__At = np.array([
@@ -71,17 +66,24 @@ class KalmanFilter:
     def __update_Tt(self, Tt):
         self.__Tt = Tt
 
+    def MU_cache(self):
+        return self.__x_MU_cache, self.__sigma_MU_cache
+    
+    def TU_cache(self):
+        return self.__x_TU_cache, self.__sigma_TU_cache
+
     def __measurement_update(self, y_t):
-        temp = self.__C@self.__sigma_t_plus1_t@self.__C.T + self.__R
-        temp = 1/temp
-        temp = temp*self.__sigma_t_plus1_t@self.__C.T
-        self.__x_hat_t_t = self.__x_hat_t_plus1_t + temp@(y_t - self.__C@self.__x_hat_t_plus1_t)
-        self.__sigma_t_t = self.__sigma_t_plus1_t - temp@self.__C@self.__sigma_t_plus1_t
+        # this is just a number
+        the_inv = self.__C@self.__sigma_TU@self.__C.T + self.__R
+        the_inv = 1/the_inv
+        temp = the_inv*self.__sigma_TU@self.__C.T
+        self.__x_MU = self.__x_TU + temp@(y_t - self.__C@self.__x_TU)
+        self.__sigma_MU = self.__sigma_TU - temp@self.__C@self.__sigma_TU
         self.__cache_MU_values()
     
     def __time_update(self):
-        self.__x_hat_t_plus1_t = self.__At@self.__x_hat_t_t
-        self.__sigma_t_plus1_t = self.__At@self.__sigma_t_t@self.__At.T + self.__Q
+        self.__x_TU = self.__At@self.__x_MU
+        self.__sigma_TU = self.__At@self.__sigma_MU@self.__At.T + self.__Q
         self.__cache_TU_values()
 
     def run(self, Tt, y_t):
@@ -89,18 +91,5 @@ class KalmanFilter:
         self.__update_At()
         self.__measurement_update(y_t)
         self.__time_update()
-        return self.__x_hat_t_t
-    
-
-if __name__ == "__main__":
-    kf = KalmanFilter()
-    Tt = np.sin(100) + np.random.normal(0, 0.1, 100)
-    y_t = np.cos(100) + np.random.normal(0, 0.1, 100)
-    x_hat_t = np.zeros([100, 4, 1])
-    for i in range(100):
-        x_hat_t[i, :, :] = kf.run(Tt[i], y_t[i])
-
-    plt.plot(x_hat_t[:, 0, 0])
-    plt.plot(y_t)
-    plt.legend([r"$\hat{x}_t$", r"$y_t$"])
-    plt.show()
+        return self.__x_MU
+ 
