704279e5c44dc6dc3a0cf444a7f6083b51ebd972
gunturiCM <gunturicm@gmail.com>
2023-06-15 00:35:22 +0100

    fixed mismatch between simulation sampling frequency and Kalman filter sampling frequency. Now both are set to 10Hz.

--- raspberry/Kalman_filter_test.py
+++ raspberry/Kalman_filter_test.py
@@ -6,14 +6,14 @@ from estimators.altitude_Kalman_filter import KalmanFilter
 
 
 num_data_points_collected = 100
-sampling_frequency = 40
+sampling_frequency = 10
 sampling_time = 1./sampling_frequency
 time_stamps = [i*sampling_time for i in range(num_data_points_collected)]
 
 
 # Simulate the dynamics and fake the measurement data for testing the least squares estimations
 dynamics = AltitudeDynamics(sampling_time=sampling_time)
-Tref_t = [20*np.sin(0.1*t) for t in range(num_data_points_collected)]
+Tref_t = [20*np.sin(0.1*t) + 20*np.cos(1*t) for t in range(num_data_points_collected)]
 for i in range(num_data_points_collected - 1):
     dynamics.simulate(Tref_t=Tref_t[i])
 
@@ -22,13 +22,13 @@ kf = KalmanFilter(initial_Tt=Tref_t[0], cache_values=True)
 
 y_t = np.zeros(num_data_points_collected)
 for i in range(num_data_points_collected):
-    y_t[i] = dynamics.z[i]
+    y_t[i] = dynamics.z[i] + np.random.normal(0, 0.1)
     kf.run(Tref_t[i], y_t[i]).reshape(4, )
 
 
 x_hat_t, _ = kf.MU_cache()
 x_hat_t = np.array(x_hat_t).reshape((num_data_points_collected, 4))
-print("alpha hat = %f"%np.max(x_hat_t[:, 2]), "\nc hat = %f"%np.max(x_hat_t[:, 3]))
+print("alpha hat = %f"%x_hat_t[-1, 2], "\nc hat = %f"%x_hat_t[-1, 3])
 
 fig, sub_plts = plt.subplots(2)
 
