0cf9aa3e7f2c1bc018758587ad3e1af3e178666b
gunturiCM <gunturicm@gmail.com>
2023-07-20 02:00:27 +0100

    parameter estimation using actual flight data.

--- simulations/estimate_SS_parameters_by_LSM_from_flight_data.py
+++ simulations/estimate_SS_parameters_by_LSM_from_flight_data.py
@@ -1,26 +1,28 @@
 # TODO: Documentation
 
-from altitude_dynamics import AltitudeDynamics
-
 import cvxpy as cp
 from matplotlib import pyplot as plt
 import numpy as np
+import pandas as pd
 
 
-num_data_points_collected = 100
-sampling_frequency = 40
+# load collected flight data
+flight_data = pd.read_csv("total_flight_data.csv", header=None).to_numpy(copy=True)
+Tref_t = flight_data[:, 1]
+altitude = flight_data[:, 5]
+
+indices_of_actual_flight = altitude > 50
+
+Tref_t = (Tref_t[indices_of_actual_flight] - 1000)/1000
+altitude = altitude[indices_of_actual_flight]
+
+num_data_points_collected = len(Tref_t)
+sampling_frequency = 50
 
 
 sampling_time = 1./sampling_frequency
 time_stamps = [i*sampling_time for i in range(num_data_points_collected)]
 
-# Simulate the dynamics and fake the measurement data for testing
-# the least squares estimations
-dynamics = AltitudeDynamics(sampling_time=sampling_time)
-Tref_t = [20*np.sin(0.1*t) for t in range(num_data_points_collected)]
-print(Tref_t)
-for i in range(num_data_points_collected - 1):
-    dynamics.simulate(Tref_t=Tref_t[i])
 
 alpha_hat = cp.Variable(shape=(1), name="alpha_hat")
 c_hat = cp.Variable(shape=(1), name="c_hat")
@@ -34,7 +36,7 @@ for t in range(num_data_points_collected - 1):
     output_error = np.random.normal(0, 1.5)
     process_error = np.random.normal(loc=0, scale=0.08*sampling_time, size=2)
     y = z_hat_t[t] + output_error
-    cost_function += (y - dynamics.z[t])**2
+    cost_function += (y - altitude[t])**2
     cstr1 = z_hat_t[t + 1] - \
         (z_hat_t[t] + sampling_time * vz_hat_t[t]) + process_error[0]
     cstr2 = vz_hat_t[t + 1] - (vz_hat_t[t] + sampling_time *
@@ -54,14 +56,14 @@ fig, sub_plts = plt.subplots(2)
 plt.rcParams["font.size"] = 16
 
 sub_plts[0].grid(True)
-sub_plts[0].plot(time_stamps, dynamics.z)
+sub_plts[0].plot(time_stamps, altitude)
 sub_plts[0].plot(time_stamps, z_hat_t.value.T)
 sub_plts[0].legend(["$z_t$", "$\hat{z}_t$"])
 sub_plts[0].set_xlabel("time s")
 sub_plts[0].set_ylabel("altitude m")
 
 sub_plts[1].grid(True)
-sub_plts[1].plot(time_stamps, dynamics.vz)
+# sub_plts[1].plot(time_stamps, altitude)
 sub_plts[1].plot(time_stamps, vz_hat_t.value.T)
 sub_plts[1].legend([r"$v_{z, t}$", r"$\hat{v}_{z, t}$"])
 sub_plts[1].set_xlabel("time s")
