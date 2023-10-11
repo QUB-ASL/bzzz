8d7cd3cbabbc39c267f8d9f9ed07a4732ab32669
gunturicm <gunturicm@gmail.com>
2023-10-11 08:23:22 +0100

    Update capital letters

--- raspberry/anemometer/read_trisonica_mini_data_rolling.py
+++ raspberry/anemometer/read_trisonica_mini_data_rolling.py
@@ -3,8 +3,8 @@ import serial
 import numpy as np
 
 all_data = []
-wind_speed = []
-wind_speed_2D = []
+Wind_Speed = []
+Wind_Speed_2D = []
 H_direction =[]
 V_direction = []
 U_axis = []
@@ -12,8 +12,8 @@ V_axis = []
 W_axis = []
 
 for x in range(20):
-   wind_speed.append(0.00)
-   wind_speed_2D.append(0.00)
+   Wind_Speed.append(0.00)
+   Wind_Speed_2D.append(0.00)
    H_direction.append(0.00)
    V_direction.append(0.00)
    U_axis.append(0.00)
@@ -31,16 +31,16 @@ if __name__ == '__main__':
 
             all_data = line.split()
 
-            wind_speed = np.roll(wind_speed, 1)
-            wind_speed_2D = np.roll(wind_speed_2D, 1)
+            Wind_Speed = np.roll(Wind_Speed, 1)
+            Wind_Speed_2D = np.roll(Wind_Speed_2D, 1)
             H_direction = np.roll(H_direction, 1)
             V_direction = np.roll(V_direction, 1)
             U_axis = np.roll(U_axis, 1)
             V_axis = np.roll(V_axis, 1)
             W_axis = np.roll(W_axis, 1)
 
-            wind_speed[0] = float(all_data[0])
-            wind_speed_2D[0] = float(all_data[1])
+            Wind_Speed[0] = float(all_data[0])
+            Wind_Speed_2D[0] = float(all_data[1])
             H_direction[0] = float(all_data[2])
             V_direction[0] = float(all_data[3])
             U_axis = float(all_data[4])
@@ -48,8 +48,8 @@ if __name__ == '__main__':
             W_axis = float(all_data[6])
 
             # print(all_data)
-            # print(wind_speed)
-            # print(wind_speed_2D)
+            # print(Wind_Speed)
+            # print(Wind_Speed_2D)
             # print(H_direction)
             # print(V_direction)
             # print(U_axis)
