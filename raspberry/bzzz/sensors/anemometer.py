4f12a86e0dce040bcc1224c73162478336c4b6dd
Pantelis Sopasakis <p.sopasakis@gmail.com>
2023-11-21 17:05:28 +0000

    anemometer: read serial in background keep data in buffer

--- raspberry/bzzz/sensors/anemometer.py
+++ raspberry/bzzz/sensors/anemometer.py
@@ -1,15 +1,18 @@
 import serial
+import numpy as np
 from threading import Thread
 
 
 class Anemometer:
 
     def __init__(self, serial_path='/dev/ttyS0', baud=115200):
-        thread = Thread(target=self.get_measurements_in_background, args=[
-                        serial_path, baud])
+        thread = Thread(target=self.get_measurements_in_background,
+                        args=[serial_path, baud])
         thread.start()
         self.__split_data_float = None
-        self.__values_cache = None
+        self.__window_length = 5
+        self.__values_cache = np.zeros((self.__window_length, 7))
+        self.__cursor = 0
 
     def get_measurements_in_background(self, serial_path, baud):
         ser = serial.Serial(serial_path, baud, timeout=1)
@@ -17,8 +20,13 @@ class Anemometer:
         while True:
             if ser.in_waiting > 0:
                 sensor_data = ser.readline().decode('utf-8').strip()
-                spilt_data = sensor_data.split()
-                self.__split_data_float = [float(x) for x in spilt_data]
+                split_data = sensor_data.split()
+                self.__split_data_float = np.array(
+                    [float(x) for x in split_data],
+                    dtype=np.float64)
+                self.__values_cache[self.__cursor, :] = self.__split_data_float
+                self.__cursor = (self.__cursor + 1) % self.__window_length
+                print(self.__values_cache)
 
     # @property
     # def all_sensor_data(self):
