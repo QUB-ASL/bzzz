ff4eb15de740c68b0ea138061e5ec248e4906223
Pantelis Sopasakis <p.sopasakis@gmail.com>
2023-11-21 16:32:34 +0000

    anemometer: thread runs in the the background connects to serial and collects data

--- raspberry/bzzz/sensors/anemometer.py
+++ raspberry/bzzz/sensors/anemometer.py
@@ -1,61 +1,66 @@
 import serial
+from threading import Thread
+
 
 class Anemometer:
-    
+
     def __init__(self, serial_path='/dev/ttyS0', baud=115200):
-        self.__ser = serial.Serial(serial_path, baud, timeout=1)
-        self.__ser.reset_input_buffer()
+        thread = Thread(target=self.get_measurements_in_background, args=[
+                        serial_path, baud])
+        thread.start()
         self.__split_data_float = None
-    
-    def get(self):
+        self.__values_cache = None
+
+    def get_measurements_in_background(self, serial_path, baud):
+        ser = serial.Serial(serial_path, baud, timeout=1)
+        ser.reset_input_buffer()
         while True:
-            if self.__ser.in_waiting > 0:
-                sensor_data = self.__ser.readline().decode('utf-8').strip()
-                spilt_data = sensor_data.split()    
-                self.__split_data_float = [float(x) for x in spilt_data]    
-                return  
+            if ser.in_waiting > 0:
+                sensor_data = ser.readline().decode('utf-8').strip()
+                spilt_data = sensor_data.split()
+                self.__split_data_float = [float(x) for x in spilt_data]
 
-    @property
-    def get_all_sensor_data(self):
-        if self.__split_data_float is not None:
-            return self.__split_data_float
-            
-    @property
-    def get_3D_wind_speed(self):
-        if self.__split_data_float is not None:
-            return self.__split_data_float[0]
+    # @property
+    # def all_sensor_data(self):
+    #     if self.__split_data_float is not None:
+    #         return self.__split_data_float
 
-    @property
-    def get_2D_wind_speed(self):
-        if self.__split_data_float is not None:
-            return self.__split_data_float[1]
+    # @property
+    # def wind_speed_3d(self):
+    #     if self.__split_data_float is not None:
+    #         return self.__split_data_float[0]
 
-    @property
-    def get_horizontal_wind_direction(self):
-        if self.__split_data_float is not None:
-            return self.__split_data_float[2]
+    # @property
+    # def wind_speed_2d(self):
+    #     if self.__split_data_float is not None:
+    #         return self.__split_data_float[1]
 
-    @property
-    def get_vertical_wind_direction(self):
-        if self.__split_data_float is not None:
-            return self.__split_data_float[3]
+    # @property
+    # def horizontal_wind_direction(self):
+    #     if self.__split_data_float is not None:
+    #         return self.__split_data_float[2]
 
-    @property
-    def get_wind_velocities(self):
-        if self.__split_data_float is not None:
-            return self.__split_data_float[-3:]
+    # @property
+    # def vertical_wind_direction(self):
+    #     if self.__split_data_float is not None:
+    #         return self.__split_data_float[3]
 
-    def update_sensor_data(self):
-        current_all_sensor_data = self.__split_data_float
-        if self.__split_data_float != self.__split_data_float:
-            return self.__split_data_float
+    # @property
+    # def wind_velocities(self):
+    #     if self.__split_data_float is not None:
+    #         return self.__split_data_float[-3:]
+
+    # def update_sensor_data(self):
+    #     current_all_sensor_data = self.__split_data_float
+    #     if self.__split_data_float != self.__split_data_float:
+    #         return self.__split_data_float
+
 
 if __name__ == '__main__':
     sensor = Anemometer()
-    for i in range(10):
-        sensor.get()
-        x = sensor.get_all_sensor_data
-        y = sensor.get_wind_velocities
-        print(x)
-        print(y)
-
+    # for i in range(10):
+    #     sensor.get_measurements_in_background()
+    #     x = sensor.all_sensor_data
+    #     y = sensor.wind_velocities
+    #     print(x)
+    #     print(y)
