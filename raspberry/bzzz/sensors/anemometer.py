5a827a530e8de4574e409d4125cd9d60bbe8ca12
Pantelis Sopasakis <p.sopasakis@gmail.com>
2023-11-22 21:01:05 +0000

    anemometer: some API docs

--- raspberry/bzzz/sensors/anemometer.py
+++ raspberry/bzzz/sensors/anemometer.py
@@ -6,7 +6,9 @@ import time
 import datetime
 import csv
 
+
 class DataProcessor(abc.ABC):
+    """Abstract data processor"""
 
     def __init__(self):
         pass
@@ -17,6 +19,7 @@ class DataProcessor(abc.ABC):
 
 
 class AverageFilter(DataProcessor):
+    """Moving average filter"""
 
     def __init__(self):
         super().__init__()
@@ -26,6 +29,11 @@ class AverageFilter(DataProcessor):
 
 
 class MedianFilter(DataProcessor):
+    """
+    Median filter
+
+    The median filter is recommended to be used when the window size is odd.
+    """
 
     def __init__(self):
         super().__init__()
@@ -35,6 +43,7 @@ class MedianFilter(DataProcessor):
 
 
 class NoFilter(DataProcessor):
+    """No filter: the latest data point is returned"""
 
     def __init__(self):
         super().__init__()
@@ -42,16 +51,20 @@ class NoFilter(DataProcessor):
     def process(self, data, cursor):
         return data[cursor]
 
+
 class DataLogger:
+    """A data logger to be used with a sensor"""
 
     def __init__(self, num_features, max_samples=1e4, feature_names=None):
-        self.__data_vault = np.zeros((max_samples, num_features), dtype=np.float64)
-        self.__timestamps_vault = np.array([datetime.datetime.now()] * max_samples)
+        self.__data_vault = np.zeros(
+            (max_samples, num_features), dtype=np.float64)
+        self.__timestamps_vault = np.array(
+            [datetime.datetime.now()] * max_samples)
         self.__feature_names = feature_names
         self.__cursor = 0
-    
+
     def record(self, timespamp, datum):
-        self.__data_vault[self.__cursor, :] = datum        
+        self.__data_vault[self.__cursor, :] = datum
         self.__cursor = self.__cursor + 1
 
     def save_to_csv(self, filename):
@@ -60,7 +73,8 @@ class DataLogger:
             writer.writerow(self.__feature_names)
         with open(filename, "a+") as fh:
             writer = csv.writer(fh)
-            data_to_record = np.hstack((np.reshape(self.__timestamps_vault[:self.__cursor], (self.__cursor, 1)), self.__data_vault[:self.__cursor, :]))
+            data_to_record = np.hstack((np.reshape(self.__timestamps_vault[:self.__cursor], (
+                self.__cursor, 1)), self.__data_vault[:self.__cursor, :]))
             writer.writerows(data_to_record)
 
 
@@ -84,6 +98,10 @@ class Anemometer:
         :param baud: baud rate of serial communication; defaults to 115200
         :param window_length: length of window of measurements; default: 3
         :param data_processor: data processor on buffer of measurements; default: MedianFilter()
+        :param log_file: file name to log data; default: None 
+
+        If `log_file` is None, the data is not logged; otherwise, on exit, 
+        the data are stored in a CSV file
 
         Note: We assume that we receive 7 measurements from the anemometer
         """
@@ -91,7 +109,7 @@ class Anemometer:
         # while the thread in the background is writing it
         self.__lock = Lock()
         self.__thread = Thread(target=self.__get_measurements_in_background_t,
-                               args=[serial_path, baud])        
+                               args=[serial_path, baud])
         self.__keep_going = True
         self.__window_length = window_length
         self.__values_cache = np.tile(np.nan, (self.__window_length, 7))
@@ -99,15 +117,21 @@ class Anemometer:
         self.__data_processor = data_processor
         self.__log_file = log_file
         if log_file is not None:
-            self.__logger = DataLogger(num_features=7, 
-                                       max_samples=100000, 
-                                       feature_names=("Date_Time", "Wind_Speed", "Wind_Speed_2D", "H_direction", "V_direction", "U_axis", "V_axis", "W_axis"))
+            feature_names = ("Date_Time", "Wind_Speed", "Wind_Speed_2D",
+                             "H_direction", "V_direction", "U_axis",
+                             "V_axis", "W_axis")
+            self.__logger = DataLogger(num_features=7,
+                                       max_samples=100000,
+                                       feature_names=feature_names)
         self.__thread.start()
 
     def __get_measurements_in_background_t(self, serial_path, baud):
         """
         This is a thread that runs in the background to connect to the 
         serial and collect measurements, which are stored in a buffer
+
+        :param serial_path: serial path
+        :param baud: baud rate of serial communication
         """
         ser = serial.Serial(serial_path, baud, timeout=1)
         ser.reset_input_buffer()
@@ -118,17 +142,17 @@ class Anemometer:
                 split_data_float = np.array(
                     [float(x) for x in split_data],
                     dtype=np.float64)
-                with self.__lock:                    
-                    self.__values_cache[self.__cursor, :] = split_data_float                    
+                with self.__lock:
+                    self.__values_cache[self.__cursor, :] = split_data_float
                     if self.__log_file is not None:
-                        data_to_log = self.__data_processor.process(self.__values_cache[:, :], cursor=self.__cursor)
+                        data_to_log = self.__data_processor.process(
+                            self.__values_cache[:, :], cursor=self.__cursor)
                         current_timestamp = datetime.datetime.now()
                         self.__logger.record(current_timestamp, data_to_log)
                     self.__cursor = (self.__cursor + 1) % self.__window_length
                 if not self.__keep_going:
                     ser.close()
                     return
-                
 
     def __enter__(self):
         return self
@@ -137,8 +161,6 @@ class Anemometer:
         self.__keep_going = False
         time.sleep(0.05)
         self.__logger.save_to_csv(self.__log_file)
-
-
 
     @property
     def all_sensor_data(self):
@@ -174,11 +196,10 @@ class Anemometer:
 if __name__ == '__main__':
 
     processor = NoFilter()
-    with Anemometer(window_length=5, 
-                    data_processor=processor, 
+    with Anemometer(window_length=5,
+                    data_processor=processor,
                     log_file="out.csv") as sensor:
         time.sleep(0.5)
         for i in range(20):
             print(sensor.all_sensor_data)
             time.sleep(0.05)
-    
