64c5993c6acfad0478be68ba494da7cba11a828f
Pantelis Sopasakis <p.sopasakis@gmail.com>
2023-11-30 15:03:16 +0000

    filters.py: minor

--- raspberry/bzzz/sensors/filters.py
+++ raspberry/bzzz/sensors/filters.py
@@ -53,12 +53,14 @@ class NoFilter(DataProcessor):
         return data[cursor]
 
 
-def median_filter(data_list: list, window_length = 5):
+def median_filter(data_list: list, window_length=5):
     """Applies median filter of given window length.
 
     :param data_list: Data list on which the filter is to be applied.
     :param window_length: filter window length, must be and int, defaults to 5
     :return: retruns the median of the given data.
+
+    ***THIS FUNCTION IS DEPRECATES! USE MedianFilter INSTEAD!***
     """
     data_list = data_list[-window_length:]  # take the latest values
     data_list.sort()
