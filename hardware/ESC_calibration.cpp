a4f159f8a23047db0ef8ee1bfc52a9dedb25a8ad
Vikram Kannur <148599421+Runway27@users.noreply.github.com>
2023-12-01 15:26:38 +0000

    Update ESC_calibration.cpp

--- hardware/ESC_calibration.cpp
+++ hardware/ESC_calibration.cpp
@@ -1,14 +1,16 @@
+
 #include <Arduino.h>
 #include "motors.hpp"
  
 bzzz::MotorDriver motorDriver;
-#define PERIOD_1 5000.
+// sets the delay value to 5 sec
+#define PERIOD_1 5000. 
 void setup()
 {
     motorDriver.attachEscToPwmPin();
-    motorDriver.writeSpeedToEsc(2000, 2000, 2000, 2000);
+    motorDriver.writeSpeedToEsc(2000, 2000, 2000, 2000); // Sets the signal to ESC as high
     delay(PERIOD_1);
-    motorDriver.writeSpeedToEsc(1000, 1000, 1000, 1000);
+    motorDriver.writeSpeedToEsc(1000, 1000, 1000, 1000); // Sets the signal to ESC as low
     delay(PERIOD_1);
 }
  
