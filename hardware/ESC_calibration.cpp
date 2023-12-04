e4f5a4df33fe47ecb65a6f72eef6ee890859602c
Runway27 <vkannur01@qub.ac.uk>
2023-12-04 14:39:35 +0000

    Changed the variable name

--- hardware/ESC_calibration.cpp
+++ hardware/ESC_calibration.cpp
@@ -5,15 +5,15 @@ bzzz::MotorDriver motorDriver;
 // sets the delay value to 5 sec
 #define DELAY_BETWEEN_HIGH_LOW_SIGNALS 5000.
 // Defining the signal high parameter
-#define High_Sgn 2000.
+#define HIGH_SGN 2000.
 // Defining the signal low parameter
-#define Low_Sgn 1000.
+#define LOW_SGN 1000.
 void setup()
 {
     motorDriver.attachEscToPwmPin();
-    motorDriver.writeSpeedToEsc(High_Sgn, High_Sgn, High_Sgn, High_Sgn); // Sets the signal to ESC as high
+    motorDriver.writeSpeedToEsc(HIGH_SGN, HIGH_SGN, HIGH_SGN, HIGH_SGN); // Sets the signal to ESC as high
     delay(DELAY_BETWEEN_HIGH_LOW_SIGNALS);
-    motorDriver.writeSpeedToEsc(Low_Sgn, Low_Sgn, Low_Sgn, Low_Sgn); // Sets the signal to ESC as low
+    motorDriver.writeSpeedToEsc(LOW_SGN, LOW_SGN, LOW_SGN, LOW_SGN); // Sets the signal to ESC as low
     delay(DELAY_BETWEEN_HIGH_LOW_SIGNALS);
 }
  
