4dbc21f81639b9f6b2bc6cb1b229072e328369f3
Jamie Rainey <jamesrainey54@hotmail.co.uk>
2024-01-22 09:43:46 +0000

    started to make data editor for simulations

---
+++ wind_forecasting_simulations/data_editor_for_simulations.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+def DataEditor(file_name):
+
+    #read data
+    df_wind = pd.read_csv(f'{file_name}.csv')
+    
+    res['Index'] = df_wind.index.values
+    res['Date_Time'] = df_wind.date_time.values
+    res['Wind_Speed'] = df_wind.wind_speed.values
+    res['Wind_Speed_2D'] = df_wind.wind_speed_2D.values
+    res['H_direction'] = df_wind.H_direction.values
+    res['V_direction'] = df_wind.V_direction.values
+    res['U_axis'] = df_wind.U_axis.values
+    res['V_axis'] = df_wind.V_axis.values
+    res['W_axis'] = df_wind.W_axis.values
+
+    res['Index_2'] = df_wind.index.values
+
+    res = res[["Index", "Index_2", "Date_Time", "Wind_Speed", "Wind_Speed_2D", "H_direction", "V_direction", "U_axis", "V_axis", "W_axis"]]
+    
+    #set index
+    res.index = pd.date_range(res.Index_2[0], res.Index_2.iloc[-1], freq="25L")
+    
+    #save file
+    res.to_csv(f'{file_name}_1.csv', index=False)
+
+
+DataEditor(file_name='raspberry/data/wind_data/12-09-23--22-02')
