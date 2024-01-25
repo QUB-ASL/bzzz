7cc8c90965a72ab47133b04b3745a718f4cd6bc3
Jamie Rainey <jamesrainey54@hotmail.co.uk>
2024-01-25 10:59:24 +0000

    updared attitude gains

--- include/config.hpp
+++ include/config.hpp
@@ -69,14 +69,14 @@
 #define RADIO_TRIMMER_MAX_QUATERNION_Z_GAIN 100.
 
 /** Trimmer C on RC - maximum omega xy gain */
-#define RADIO_TRIMMER_MAX_OMEGA_XY_GAIN 0.5
+#define RADIO_TRIMMER_MAX_OMEGA_XY_GAIN 0.3
 /** Tested attitude angular velocity roll-pitch axis control gain*/
-#define OMEGA_XY_GAIN 0.266
+#define OMEGA_XY_GAIN 0.305
 
 /** Trimmer E on RC - maximum omega z gain */
-#define RADIO_TRIMMER_MAX_OMEGA_Z_GAIN 10
+#define RADIO_TRIMMER_MAX_OMEGA_Z_GAIN 4
 /** Tested attitude angular velocity yaw axis control gain*/
-#define OMEGA_Z_GAIN 0.145
+#define OMEGA_Z_GAIN 0.111
 
 /**
  * Control action to PWM scaling factor
