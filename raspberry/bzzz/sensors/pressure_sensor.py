899de7b425e4fa16a1beccd2af4fcad253152112
Jamie Rainey <jamesrainey54@hotmail.co.uk>
2024-03-21 18:50:01 +0000

    implementation of pressure sensor initialisation done

--- raspberry/bzzz/sensors/pressure_sensor.py
+++ raspberry/bzzz/sensors/pressure_sensor.py
@@ -7,56 +7,69 @@ from threading import Thread, Lock
 from data_logger import DataLogger
 import datetime
 
-def getShort(data, index):
-    """
-    Extract a signed 16-bit value from a data array starting at the specified index.
-    :param data: A list or tuple containing byte data.
-    :param index: The starting index in the data array.
-    :return: A signed 16-bit integer value.
-    """
-    value = data[index] * 256 + data[index + 1]
-    return value - 65536 if value > 32767 else value
-class BMP180Sensor:
-    def __init__(self, data_processor, window_length, DEVICE_ADDRESS=0x77, reference_pressure_at_sea_level=101325, log_file=None):
-        self.daemon = True
-        self.__lock = Lock()
-        self.__thread = Thread(target=self.__get_measurements_in_background)
-        self.window_length = window_length
-        self.DEVICE_ADDRESS = DEVICE_ADDRESS
-        self.bus = smbus.SMBus(1)
-        self.data_processor = data_processor
-        self.reference_pressure_at_sea_level = reference_pressure_at_sea_level
-        self.__current_pressure = None
-        self.__pressure_readings_list = []
-        self.__current_altitude = None
-        self.__calibrated = False  # Initialization of the __calibrated attribute
-        self.__altitude_initilisation = None
-        
-        self.log_file = log_file
-        if log_file is not None:
-            feature_names = ("Date_Time", "Pressure", "Altitude")
-            self.logger = DataLogger(num_features=2, feature_names=feature_names)
-        self.__thread.start()
-    def __get_measurements_in_background(self):
-        while True:
-            self.update_pressure()
-            self.__calculate_altitude()
-            if self.log_file is not None:
-                current_timestamp = datetime.datetime.now()
-                data_to_log = (self.__current_pressure, self.__current_altitude)
-                self.logger.record(current_timestamp, data_to_log)
-            time.sleep(0.5)  # Delay between measurements
-    def update_pressure(self):
-        with self.__lock:
-            pressure_reading = self.get_pressure_measurement()
-            self.__pressure_readings_list.append(pressure_reading)
-            if len(self.__pressure_readings_list) > self.window_length:
-                self.__pressure_readings_list.pop(0)
-            self.__current_pressure = self.data_processor.process(self.__pressure_readings_list)
 
-    def get_pressure_measurement(self):
+def _calculate_altitude(pres, temp, reference_pressure_at_sea_level):
+    k = 1.380649e-23 #Boltzmann's Number
+    m = 4.8e-26 #mass of one molicule of air 
+    g = 9.81 #gravity
+    alt = (((temp+273.15)*k)/((m)*g)) * math.log(reference_pressure_at_sea_level/pres)
+    return alt
+    
+class BMP180Sensor:
+
+    def __init__(self, 
+                 data_processor=MedianFilter(), 
+                 window_length=3, 
+                 DEVICE_ADDRESS=0x77, 
+                 reference_pressure_at_sea_level=101325, 
+                 log_file=None):
+        self.__lock = Lock()
+        self.__keep_running = True
+        self.__thread = Thread(target=self.__get_measurements_in_background)
+        self.__window_length = window_length
+        self.__values_cache = np.tile(np.nan, (self.__window_length, 2))
+        self.__cursor = 0
+        self.__DEVICE_ADDRESS = DEVICE_ADDRESS
+        self.__bus = smbus.SMBus(1)
+        self.__data_processor = data_processor
+        self.__reference_pressure_at_sea_level = reference_pressure_at_sea_level
+                
+        self.__log_file = log_file
+        if self.__log_file is not None:
+            feature_names = ("Date_Time", "Pressure", "Altitude")
+            self.__logger = DataLogger(num_features=2, feature_names=feature_names)
+        self.__thread.start()
+        self.__av_pressure = -1
+        av = self.__initialise()
+        self.__av_pressure = av
+
+    def __pressure_correction(self, p):
+        delta = self.__av_pressure - self.__reference_pressure_at_sea_level
+        return p - delta
+    
+    def __get_measurements_in_background(self):
+        while self.__keep_running:
+            pres_raw, temp = self.__pressure_temp_from_sensor()
+            pres = self.__pressure_correction(pres_raw) if self.__av_pressure > 0 else pres_raw
+            alt = _calculate_altitude(pres, temp, self.__reference_pressure_at_sea_level) 
+            self.__values_cache[self.__cursor, :] = np.array([pres, alt])
+            self.__cursor = (self.__cursor + 1) % self.__window_length
+            if self.__log_file is not None and self.__av_pressure > 0:
+                current_timestamp = datetime.datetime.now()
+                data_to_log = (pres, alt)
+                self.__logger.record(current_timestamp, data_to_log)  
+
+    def pressure(self):
+        with self.__lock:
+            return self.__data_processor.process(self.__values_cache[:, :], cursor=self.__cursor)[0]
+    
+    def altitude(self):
+        with self.__lock:
+            return self.__data_processor.process(self.__values_cache[:, :], cursor=self.__cursor)[1]
+
+    def __pressure_temp_from_sensor(self):
         # Reading calibration data
-        data = self.bus.read_i2c_block_data(self.DEVICE_ADDRESS, 0xAA, 22)
+        data = self.__bus.read_i2c_block_data(self.__DEVICE_ADDRESS, 0xAA, 22)
 
         # Converting calibration data
         AC1 = data[0] * 256 + data[1]
@@ -87,15 +100,15 @@ class BMP180Sensor:
         if MD > 32767 :
             MD -= 65535
         # Reading raw temperature
-        self.bus.write_byte_data(self.DEVICE_ADDRESS, 0xF4, 0x2E)
+        self.__bus.write_byte_data(self.__DEVICE_ADDRESS, 0xF4, 0x2E)
         time.sleep(0.005)
-        data = self.bus.read_i2c_block_data(self.DEVICE_ADDRESS, 0xF6, 2)
+        data = self.__bus.read_i2c_block_data(self.__DEVICE_ADDRESS, 0xF6, 2)
         UT = data[0] * 256 + data[1]
 
         # Reading raw pressure
-        self.bus.write_byte_data(self.DEVICE_ADDRESS, 0xF4, 0x74)
+        self.__bus.write_byte_data(self.__DEVICE_ADDRESS, 0xF4, 0x74)
         time.sleep(0.026)
-        data = self.bus.read_i2c_block_data(self.DEVICE_ADDRESS, 0xF6, 3)
+        data = self.__bus.read_i2c_block_data(self.__DEVICE_ADDRESS, 0xF6, 3)
         UP = ((data[0] * 65536) + (data[1] * 256) + data[2]) / 128
 
         # Calculating true temperature
@@ -124,56 +137,44 @@ class BMP180Sensor:
         X1 = (X1 * 3038.0) / 65536.0
         X2 = ((-7357) * pressure) / 65536.0
         pressure = (pressure + (X1 + X2 + 3791) / 16.0)
-        return pressure
-
-    def __calculate_altitude(self):
-        with self.__lock:
-            temp = 20 #temp in celcius 
-            k = 1.380649e-23 #Boltzmann's Number
-            m = 4.8e-26 #mass of one molicule of air 
-            g = 9.81 #gravity
-            self.__current_altitude = (((temp+273.15)*k)/((m)*g)) * math.log(self.reference_pressure_at_sea_level/self.__current_pressure)
-            self.__current_altitude 
+        return pressure, T
+              
             
-    def altitude_initialization(self, num_initial_readings=60):
+    def __initialise(self, num_initial_readings=30):
         """
         Initialize the altitude by averaging the first few readings.
         :param num_initial_readings: Number of readings to average for initial altitude.
-        """
-        if not self.__calibrated:
-            initial_altitudes = []
-            for _ in range(num_initial_readings):
-                self.update_pressure()  # Update pressure to get the latest reading
-                self.__calculate_altitude()  # Calculate altitude based on current pressure
-                if self.__current_altitude is not None:
-                    initial_altitudes.append(self.__current_altitude)
-                time.sleep(1)  # Wait a bit before the next reading
-
-            if initial_altitudes:
-                self.__altitude_initilisation = sum(initial_altitudes) / len(initial_altitudes)
-                self.__calibrated = True
-
-
-    def convert_to_signed(self, data):
-        value = data[0] * 256 + data[1]
-        if value > 32767:
-            value -= 65536
-        return value
-    
-    def convert_to_unsigned(self, data):
-        return data[0] * 256 + data[1]
+        """        
+        sum_pressures = 0
+        do_run = True
+        i = 0
+        time.sleep(3)
+        while do_run:
+            p = self.pressure()
+            if not np.isnan(p):
+                sum_pressures += p
+                i += 1
+            if i == num_initial_readings:
+                break
+            time.sleep(0.2)
+        av_pressure = sum_pressures / num_initial_readings
+        return av_pressure
+        
     
     def __enter__(self):
         return self
     
     def __exit__(self, exc_type, exc_val, exc_tb):
-        self.__keep_going = False
-        if self.log_file is not None:
-            self.logger.save_to_csv(self.log_file)
+        self.__keep_running = False
+        if self.__log_file is not None:
+            self.__logger.save_to_csv(self.__log_file)
 
 if __name__ == "__main__":
     log_filename = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S_BMP180Sensor.csv")
     processor = AverageFilter()
-    with BMP180Sensor(window_length=5, data_processor=processor, reference_pressure_at_sea_level=101325, log_file=log_filename) as sensor:
-        sensor.altitude_initialization()  # Initialize the starting altitude
-        time.sleep(600)  # Collect data for 10 minutes
+    with BMP180Sensor(window_length=5, log_file=log_filename) as sensor:
+        for i in range(100):
+            print(sensor.altitude())
+            time.sleep(0.1)
+
+        
