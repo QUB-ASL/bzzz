87c95d2c6c0f724899f7df1832ed8a129dbb5ec9
Jamie Rainey <jamesrainey54@hotmail.co.uk>
2024-03-24 13:22:57 +0000

    save changes

--- TOF_PLOT.py
+++ TOF_PLOT.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt 
 import pandas as pd
 
-file = pd.read_csv("Evo-ToF-15-03-24--16-44_NEW.csv")
+file = pd.read_csv("Anemometer-15-03-24--16-44.csv")
+file2 = pd.read_csv("EvoNNEEEWWW-ToF-15-03-24--16-44.csv")
 
 plt.figure(figsize = (10, 6))
-plt.plot(file.Date_Time, file.Altitude)
-plt.plot(file.Date_Time, file.Reference_altitude)
+plt.plot(file.Date_Time, file.H_direction)
+# plt.plot(file2.timestamp, file2.Altitude)
 
 plt.title('Altitude')
 #plt.xlabel('Date_time')
--- timestamp_to_datetime.py
+++ timestamp_to_datetime.py
@@ -1,26 +1,3 @@
-from datetime import datetime
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import csv
-
-
-#read data
-df = pd.read_csv('Anemometer.csv')
-
-res = df
-
-res['timestamp'] = 0
-
-for x in range(1,3361):
-    res.timestamp[x] = datetime.timestamp(pd.to_datetime(res.Date_Time.values[x])) - datetime.timestamp(pd.to_datetime(res.Date_Time.values[0]))
-
-res = res[::10]
-
-#save file
-res.to_csv('Anemometer.csv')
-
-
 # from datetime import datetime
 # import pandas as pd
 # import numpy as np
@@ -29,10 +6,40 @@ res.to_csv('Anemometer.csv')
 
 
 # #read data
-# df = pd.read_csv('All_data.csv')
+# df = pd.read_csv('Evo-ToF-15-03-24--16-44.csv')
 
-# res = df[['timestamp','KF_beta_est']].copy()
+# res = df
 
+# res['timestamp'] = 0
+
+# for x in range(1,12573):
+#     res.timestamp[x] = datetime.timestamp(pd.to_datetime(res.Date_Time.values[x])) - datetime.timestamp(pd.to_datetime(res.Date_Time.values[0]))
+#     res.timestamp[x] = res.timestamp[x] * 1000
+
+# res = res[::10]
 
 # #save file
-# res.to_csv('KF_beta_est.csv')
+# res.to_csv('EvoNNEEEWWW-ToF-15-03-24--16-44.csv')
+
+
+from datetime import datetime
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+
+#read data
+df = pd.read_csv('altitude_ref.csv')
+
+res = df
+
+for x in range(0,356):
+    res.altitude_ref[x] = float(res.altitude_ref[x])
+    
+
+res = df[['timestamp','altitude_ref']].copy()
+
+
+#save file
+res.to_csv('altitude_ref.csv')
