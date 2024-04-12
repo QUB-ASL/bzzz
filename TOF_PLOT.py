dc1f72d1bb862691cfb72ebff9fe479c2de1e480
Jamie Rainey <jamesrainey54@hotmail.co.uk>
2024-04-12 11:20:46 +0100

    update plotter

--- TOF_PLOT.py
+++ TOF_PLOT.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt 
 import pandas as pd
 
-file = pd.read_csv("Anemometer-15-03-24--16-44.csv")
+file = pd.read_csv("2024_3_15_at_16h46m37s.csv")
 file2 = pd.read_csv("EvoNNEEEWWW-ToF-15-03-24--16-44.csv")
 
 plt.figure(figsize = (10, 6))
-plt.plot(file.Date_Time, file.H_direction)
-# plt.plot(file2.timestamp, file2.Altitude)
+plt.plot(file.timestamp, file.KF_altitutde_est)
+plt.plot(file2.timestamp, file2.Altitude)
 
 plt.title('Altitude')
-#plt.xlabel('Date_time')
+plt.xlabel('Date_time')
 
 
 plt.xticks([])
