84ca9008961e0b35fdccd1f41053d95d26809073
Jamie Rainey <jamesrainey54@hotmail.co.uk>
2024-04-15 14:38:45 +0100

    add documentation

--- raspberry/bzzz/estimators/altitude_hold_kalman_filter.py
+++ raspberry/bzzz/estimators/altitude_hold_kalman_filter.py
@@ -1,9 +1,20 @@
 import numpy as np
 
 def _tilt_correction(x, pitch_rad, roll_rad):
+        """
+        Calculates the correct alttitude given 
+        pitch and roll angles in radians.
+
+        :param x: Measured alttitude. 
+        :param pitch_rad: pitch in radians.
+        :param roll_rad: roll in radians.
+        """
         return np.cos(pitch_rad) * np.cos(roll_rad) * x
 
 class AltitudeHoldKalmanFilter:
+    """
+    Class to for the altitude hold Kalman Filter.
+    """
 
     def __init__(self,            
                  initial_state,
@@ -11,6 +22,24 @@ class AltitudeHoldKalmanFilter:
                  state_cov,
                  meas_cov,
                  sampling_time=0.1):
+        """
+        Initalise the altitude hold kalman filter.
+
+        :param initial_state: Initial state of the drone.
+                              Altitude defualts to 0.5 m.
+                              Velocity defualts to 0 m/s.
+                              Alpha one defualts to 3.
+                              Alpha zero defualts to -1.
+        :param initial_sigma: 
+        :param state_cov: covariance of the states.
+                          Altitude defualts to 0.001.
+                          Velocity defualts to 0.01.
+                          Alpha one defualts to 8.0e-07.
+                          Alpha zero defualts to 1.74e-07.                 
+        :param meas_cov: Measurement/sensor covariance.
+                         ToF sensor defualts to 0.0001
+        :param sampling_time: sampling time; defalts to 0.02s.
+        """
         self.__sampling_time = sampling_time
         self.__C = np.array([[1, 0, 0, 0]])        
         self.__Q = state_cov
@@ -23,6 +52,11 @@ class AltitudeHoldKalmanFilter:
 
     
     def __matrix_a(self, tau):
+        """
+        :param tau: Throttle perecntage
+
+        Returns the A matrix
+        """
         k_t = 0.5 * self.__sampling_time**2
         return np.array([
                 [1, self.__sampling_time, k_t * self.__sampling_time, k_t],
@@ -34,7 +68,8 @@ class AltitudeHoldKalmanFilter:
                              y_t,
                              pitch_rad=0.,
                              roll_rad=0.):
-        """Does measurement update step of the Kalman filter.
+        """
+        Does measurement update step of the Kalman filter.
 
         :param y_t: Current altitude measurement.
         :param pitch_rad: current drone pitch in radians, defaults to 0.
@@ -50,13 +85,23 @@ class AltitudeHoldKalmanFilter:
         self.__sigma_meas = self.__sigma_pred - temp@self.__C@self.__sigma_pred
     
     def __time_update(self, tau):
-        """Does time update step of the kalman filter.
+        """
+        Does time update step of the kalman filter.
+        :param tau: Throttle perecntage
         """
         A = self.__matrix_a(tau)        
         self.__x_pred = A @ self.__x_meas.reshape((4, 1))
         self.__sigma_pred = A @ self.__sigma_meas @ A.T + self.__Q
     
     def update(self, tau, pitch_rad, roll_rad, y_t):
+         """
+         Kalman Filter update.
+
+         :param tau: Throttle perecntage.
+         :param y_t: Current altitude measurement.
+         :param pitch_rad: current drone pitch in radians.
+         :param roll_rad: current drone roll in radians.
+         """
          tau_corr = _tilt_correction(tau, pitch_rad, roll_rad)
          if not np.isnan(y_t):
               self.__measurement_update(y_t, pitch_rad, roll_rad)
@@ -66,12 +111,21 @@ class AltitudeHoldKalmanFilter:
          self.__time_update(tau_corr)
     
     def x_measured(self):
+         """
+         Returns the state measurments
+         """
          return self.__x_meas
 
     def sigma_measured(self):
+         """
+         Returns the sigma measurments
+         """
          return self.__sigma_meas
     
     def tau_eq_estimate(self):
+         """
+         returns the equilibrium/hovering throttle as a percentage.
+         """
          alpha_1_est, alpha_0_est = self.__x_meas[2], self.__x_meas[3]
          tau_eq_est = -alpha_0_est/alpha_1_est
          return tau_eq_est
