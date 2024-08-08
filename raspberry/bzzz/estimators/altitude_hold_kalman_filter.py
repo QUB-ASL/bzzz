7ca64dfff7c78884bfd39b8bf5923d59323a71b7
Jamie Rainey <jamesrainey54@hotmail.co.uk>
2024-08-08 17:03:39 +0100

    update Kalman filter for varrying values of R matrix  etc to match paper

--- raspberry/bzzz/estimators/altitude_hold_kalman_filter.py
+++ raspberry/bzzz/estimators/altitude_hold_kalman_filter.py
@@ -2,10 +2,10 @@ import numpy as np
 
 def _tilt_correction(x, pitch_rad, roll_rad):
         """
-        Calculates the correct alttitude given 
+        Calculates the correct altitude given 
         pitch and roll angles in radians.
 
-        :param x: Measured alttitude. 
+        :param x: Measured altitude. 
         :param pitch_rad: pitch in radians.
         :param roll_rad: roll in radians.
         """
@@ -20,58 +20,89 @@ class AltitudeHoldKalmanFilter:
                  initial_state,
                  initial_sigma,
                  state_cov,
-                 meas_cov,
+                 gnss_var_rtk,
+                 gnss_var_no_rtk,
+                 tof_var_low_alt,
+                 bar_var,
+                 tof_var_change_alt,
                  sampling_time=0.02):
         """
-        Initalise the altitude hold kalman filter.
+        Initialise the altitude hold kalman filter.
 
         :param initial_state: Initial state of the drone.
-                              Altitude defualts to 0.5 m.
-                              Velocity defualts to 0 m/s.
-                              Alpha one defualts to 3.
-                              Alpha zero defualts to -1.
+                              Altitude defaults to 0.5 m.
+                              Velocity defaults to 0 m/s.
+                              Alpha one defaults to 3.
+                              Alpha zero defaults to -1.
         :param initial_sigma: 
         :param state_cov: covariance of the states.
-                          Altitude defualts to 0.001.
-                          Velocity defualts to 0.01.
-                          Alpha one defualts to 8.0e-07.
-                          Alpha zero defualts to 1.74e-07.                 
-        :param meas_cov: Measurement/sensor covariance.
-                         ToF sensor defualts to 0.0001
-        :param sampling_time: sampling time; defalts to 0.02s.
+                          Altitude defaults to 0.001.
+                          Velocity defaults to 0.01.
+                          Alpha one defaults to 8.0e-07.
+                          Alpha zero defaults to 1.74e-07.  
+        :param sampling_time: sampling time; defaults to 0.02s.
         """
         self.__sampling_time = sampling_time
-        self.__C = np.array([[1, 0, 0, 0, 0, 0]
-                             [1, 0, 0, 0, 1, 0]
-                             [1, 0, 0, 0, 0, 1]])        
+        self.__C = np.array([[1, 0, 0, 0, 0, 0],
+                             [1, 0, 0, 0, 1, 0],
+                             [1, 0, 0, 0, 0, 1]])
+        self.__C_no_gnss = np.array([[1, 0, 0, 0, 1, 0],
+                                     [1, 0, 0, 0, 0, 1]])      
         self.__Q = state_cov
-        self.__R = meas_cov
-        self.__x_pred = initial_state    
+        self.__gnss_var_rtk = gnss_var_rtk
+        self.__gnss_var_no_rtk = gnss_var_no_rtk
+        self.__tof_var_low_alt = tof_var_low_alt
+        self.__bar_var = bar_var
+        self.__tof_var_change_alt = tof_var_change_alt
+        self.__x_pred = initial_state.reshape((6, 1)) 
         self.__sigma_pred = initial_sigma
         self.__x_meas = initial_state
         self.__sigma_meas = initial_sigma
+        self.__last_y_gnss = 0
         pass
-
     
-    def __matrix_a(self, tau):
+    def __matrix_r(self,
+                   y_t_tof,
+                   rtk_status):
         """
-        :param tau: Throttle perecntage
+        :param 
 
         Returns the A matrix
         """
-        k_t = 0.5 * self.__sampling_time**2
+        if rtk_status is True:
+             gnss_var = self.__gnss_var_rtk
+        else:
+             gnss_var = self.__gnss_var_no_rtk
+        
+        if y_t_tof <= self.__tof_var_change_alt:
+             tof_var = self.__tof_var_low_alt
+        else:
+             tof_var = np.square(0.0015*y_t_tof)
+
+        return np.array([
+                [gnss_var, 0, 0],
+                [0, tof_var, 0],
+                [0, 0, self.__bar_var]])
+    
+    def __matrix_a(self, tau):
+        """
+        :param tau: Throttle percentage
+
+        Returns the A matrix
+        """
         return np.array([
                 [1, self.__sampling_time, 0, 0, 0, 0],
-                [0, 1, self.__sampling_time * tau, self.__sampling_time, 0, 0],
+                [0, 1, self.__sampling_time, self.__sampling_time * tau, 0, 0],
                 [0, 0, 1, 0, 0, 0],
                 [0, 0, 0, 1, 0, 0],
                 [0, 0, 0, 0, 1, 0],
                 [0, 0, 0, 0, 0 ,1]])
     
-    def __measurement_update(self,
-                             y_t,
-                             pitch_rad=0.,
-                             roll_rad=0.):
+    def __measurement_update_all_sensors(self,
+                                         y_t,
+                                         rtk_status,
+                                         pitch_rad=0.,
+                                         roll_rad=0.):
         """
         Does measurement update step of the Kalman filter.
 
@@ -79,50 +110,85 @@ class AltitudeHoldKalmanFilter:
         :param pitch_rad: current drone pitch in radians, defaults to 0.
         :param roll_rad: current drone roll in radians, defaults to 0.
         """
-        # NOTE: The below line is added new and untested
-        y_t = _tilt_correction(y_t, pitch_rad, roll_rad)
-        # this is just a number
-        the_inv = self.__C @ self.__sigma_pred @ self.__C.T + self.__R
-        the_inv = 1/the_inv
-        temp = the_inv * self.__sigma_pred @ self.__C.T
-        self.__x_meas = self.__x_pred + temp @ (y_t - self.__C@self.__x_pred)
-        self.__sigma_meas = self.__sigma_pred - temp@self.__C@self.__sigma_pred
+        R = self.__matrix_r(y_t[1], rtk_status)
+        e = y_t.reshape((3, 1)) - self.__C @ self.__x_pred
+        R_tilde = self.__C @ self.__sigma_pred @ self.__C.T + R
+        self.__x_meas = self.__x_pred + ((self.__sigma_pred @ self.__C.T) 
+                                         @ np.linalg.solve(R_tilde, e))
+        self.__sigma_meas = self.__sigma_pred - (
+                    (self.__sigma_pred @ self.__C.T) 
+                    @ np.linalg.solve(R_tilde, self.__C @ self.__sigma_pred))
+    
+    def __measurement_update_no_gnss(self,
+                                     y_t,
+                                     rtk_status,
+                                     pitch_rad=0.,
+                                     roll_rad=0.):
+        """Does measurement update step of the Kalman filter.
+ 
+        :param y_t: Current altitude measurement.
+        :param pitch_rad: current drone pitch in radians, defaults to 0.
+        :param roll_rad: current drone roll in radians, defaults to 0.
+        """
+        y_t = y_t[1:3]
+        R = self.__matrix_r(y_t[0], rtk_status)
+        e = y_t.reshape((2, 1))  - self.__C_no_gnss @ self.__x_pred
+        R_tilde = self.__C_no_gnss @ self.__sigma_pred @ self.__C_no_gnss.T 
+        + R[1:3,1:3]
+        self.__x_meas = self.__x_pred + (
+                              (self.__sigma_pred @ self.__C_no_gnss.T) 
+                              @ np.linalg.solve(R_tilde, e))
+        self.__sigma_meas = self.__sigma_pred - (
+             (self.__sigma_pred @ self.__C_no_gnss.T) 
+             @ np.linalg.solve(R_tilde, self.__C_no_gnss @ self.__sigma_pred))
     
     def __time_update(self, tau):
         """
         Does time update step of the kalman filter.
-        :param tau: Throttle perecntage
+        :param tau: Throttle percentage
         """
         A = self.__matrix_a(tau)        
         self.__x_pred = A @ self.__x_meas.reshape((6, 1))
         self.__sigma_pred = A @ self.__sigma_meas @ A.T + self.__Q
     
-    def update(self, tau, pitch_rad, roll_rad, y_t):
+    def update(self, tau, pitch_rad, roll_rad, y_t, rtk_status):
          """
          Kalman Filter update.
 
-         :param tau: Throttle perecntage.
+         :param tau: Throttle percentage.
          :param y_t: Current altitude measurement.
          :param pitch_rad: current drone pitch in radians.
          :param roll_rad: current drone roll in radians.
          """
-         y_t = _tilt_correction(y_t, pitch_rad, roll_rad)
-         if not np.isnan(y_t):
-              self.__measurement_update(y_t, pitch_rad, roll_rad)
+         y_t[0] = _tilt_correction(y_t[0], pitch_rad, roll_rad)
+         y_t[1] = _tilt_correction(y_t[1], pitch_rad, roll_rad)
+         y_t[2] = _tilt_correction(y_t[2], pitch_rad, roll_rad)
+         if not np.isnan(y_t[1]):
+             if self.__last_y_gnss == y_t[0]:
+                 self.__measurement_update_no_gnss(y_t, 
+                                                   rtk_status, 
+                                                   pitch_rad, 
+                                                   roll_rad)
+             else:
+                 self.__measurement_update_all_sensors(y_t, 
+                                                       rtk_status, 
+                                                       pitch_rad, 
+                                                       roll_rad)
          else: 
               self.__x_meas = self.__x_pred
               self.__sigma_meas = self.__sigma_pred
          self.__time_update(tau)
+         self.__last_y_gnss = y_t[0]
     
     def x_measured(self):
          """
-         Returns the state measurments
+         Returns the state measurements
          """
          return self.__x_meas
 
     def sigma_measured(self):
          """
-         Returns the sigma measurments
+         Returns the sigma measurements
          """
          return self.__sigma_meas
     
@@ -130,10 +196,6 @@ class AltitudeHoldKalmanFilter:
          """
          returns the equilibrium/hovering throttle as a percentage.
          """
-         alpha_1_est, alpha_0_est = self.__x_meas[2], self.__x_meas[3]
+         alpha_0_est, alpha_1_est = self.__x_meas[2], self.__x_meas[3]
          tau_eq_est = -alpha_0_est/alpha_1_est
          return tau_eq_est
-        
-    
-    
-     
--- raspberry/main.py
+++ raspberry/main.py
@@ -6,7 +6,6 @@ import bzzz.util.constants as constants
 from time import time_ns  # function to get system-up time in nano seconds
 from bzzz.controllers.altitude_controller import *  # altitude hold controller
 from bzzz.estimators.altitude_hold_kalman_filter import *
-from bzzz.sensors.time_of_flight_sensor import TimeOfFlightSensor
 from bzzz.read_sbus import RC
 from bzzz.read_sbus.radioData import *
 from bzzz.read_sbus.esp_bridge import *
@@ -22,18 +21,23 @@ if __name__ == '__main__':
     # Global variables
     params = constants.constants()
     min_altitude_hold_altitude = params["min_altitude_hold_altitude"]
-    feature_names = ("datetime", "z", "z_ref", "z_hat",
-                     "v_hat", "alpha_1", "alpha_0", "tau")
-    logger = DataLogger(num_features=7,
+    feature_names = ("datetime", "z_gnss", "z_tof", "z_bar", "z_ref", "z_hat",
+                     "v_hat", "alpha_0", "alpha_1", "d_bar", "d_gnss", "tau")
+    logger = DataLogger(num_features=11,
                         feature_names=feature_names,
-                        max_samples=50000)
+                        max_samples=500000)
     sampling_time = params["sampling_time"]
     kf_params = params["ah_kf"]
     altitude_kf = AltitudeHoldKalmanFilter(
         initial_state=np.array(kf_params["initial_state"]),
         initial_sigma=np.diagflat(kf_params["initial_sigma"]),
         state_cov=np.diagflat(kf_params["state_cov"]),
-        meas_cov=kf_params["meas_cov"])
+        gnss_var_rtk = kf_params["gnss_var_rtk"],
+        gnss_var_no_rtk= kf_params["gnss_var_no_rtk"],
+        tof_var_low_alt= kf_params["tof_var_low_alt"],
+        bar_var= kf_params["bar_var"],
+        tof_var_change_alt= kf_params["tof_var_change_alt"],
+        sampling_time=0.02)
     altitude_ctrl = AltitudeController()
     rc = RC()
 
@@ -72,9 +76,9 @@ if __name__ == '__main__':
             return (val - 0.35)/0.35
         return 0
 
-    def altitude_estimator(radio_data, y):
+    def altitude_estimator(radio_data, y, rtk_status):
         tau = radio_data.throttle_reference_percentage()
-        altitude_kf.update(tau, 0, 0, y)
+        altitude_kf.update(tau, 0, 0, y, rtk_status)
 
     def percentage_to_throttle_radio(val):
         return 300 + 1400 * val
@@ -83,7 +87,8 @@ if __name__ == '__main__':
         vre = radio_data.trimmer_VRE_percentage()
         sc_vre = trimmer_to_altitude_increment(vre)
         meters_pre_sec_max_increment = 0.04
-        meters_pre_tick_max_increment = meters_pre_sec_max_increment * sampling_time
+        meters_pre_tick_max_increment = (meters_pre_sec_max_increment * 
+                                         sampling_time)
         increment_action = sc_vre * meters_pre_tick_max_increment
         altitude_ctrl.increment_reference(increment_action)
         altitude_ctrl.set_tau_eq(altitude_kf.tau_eq_estimate())
@@ -97,21 +102,24 @@ if __name__ == '__main__':
         radio_data.set_throttle(throttle)
 
     def save_data():
-        blackbox_fname = datetime.datetime.now().strftime("BB-%d-%m-%y--%H-%M.csv")
+        blackbox_fname = datetime.datetime.now().strftime
+        ("BB-%d-%m-%y--%H-%M.csv")
         logger.save_to_csv(blackbox_fname)
         print("All sensors are saving data; bye!")
 
     def record_black_box_data(radio_data, y):
         current_timestamp = datetime.datetime.now()
         state_est = altitude_kf.x_measured()
-        data_to_log = np.zeros((7, ))
-        data_to_log[0] = y
-        data_to_log[1] = altitude_ctrl.altitude_reference()
-        data_to_log[2:6] = state_est.reshape((4, ))
-        data_to_log[6] = radio_data.throttle_reference_percentage()
+        data_to_log = np.zeros((11, ))
+        data_to_log[0] = y[0]
+        data_to_log[1] = y[1]
+        data_to_log[2] = y[2]
+        data_to_log[3] = altitude_ctrl.altitude_reference()
+        data_to_log[4:8] = state_est.reshape((6, ))
+        data_to_log[8] = radio_data.throttle_reference_percentage()
         logger.record(current_timestamp, data_to_log)
 
-    def control_loop(tof, bar, gps, esp_bridge):
+    def control_loop(tof, barometer, gnss, esp_bridge):
         connection_lost_flag, radio_data = rc.get_radio_data()
         radio_data = RadioData(radio_data)
         do_kill = radio_data.switch_D()
@@ -127,17 +135,17 @@ if __name__ == '__main__':
         take_emergency_measure(measure, radio_data)
 
         flight_mode = radio_data.switch_C()
+        rtk_status =    gnss.rtk_status
         y_tof = tof.distance
-        y_bar = bar.altitude
-        y_gps = gps.altitude
-        y = np.array([y_tof, y_bar, y_gps])
-        
-        if y > min_altitude_hold_altitude:
-            altitude_estimator(radio_data, y)  # deals with nans
+        y_bar = barometer.altitude()
+        y_gnss = gnss.altitude
+        y = np.array([y_gnss, y_tof, y_bar])
+        if y_tof > min_altitude_hold_altitude:
+            altitude_estimator(radio_data, y, rtk_status)  # deals with nans
 
         match flight_mode:
             case ThreeWaySwitch.MID.value:
-                if y > min_altitude_hold_altitude:
+                if y_tof > min_altitude_hold_altitude:
                     altitude_control(radio_data)
             case _:
                 altitude_ctrl.set_altitude_reference(tof.distance)
@@ -149,13 +157,25 @@ if __name__ == '__main__':
     # ------------------------------------------------
     # MAIN LOOP!
     # ------------------------------------------------
+    EVO_filename = datetime.datetime.now().strftime
+    ("Evo-ToF-%d-%m-%y--%H-%M.csv")
+    BAR_filename = datetime.datetime.now().strftime
+    ("PressureSensor-%d-%m-%y--%H-%M.csv")
+    ANE_filename = datetime.datetime.now().strftime
+    ("Anemometer-%d-%m-%y--%H-%M.csv")
+    GNSS_filename = datetime.datetime.now().strftime
+    ("GNSS-%d-%m-%y--%H-%M.csv")
+    processor = MedianFilter()  # You need to define this class 
+                                # based on your requirements
     keep_running = True
-    with (EvoSensor(data_processor=MedianFilter()) as tof,
-          Anemometer() as anemometer,
-          PressureSensor() as barometer,
-          Gnss() as gnss,
+    with (EvoSensor(window_length=3,  
+                    data_processor=processor,  
+                    log_file=EVO_filename) as tof, 
+          Anemometer(log_file=ANE_filename) as anemometer,
+          BMP180Sensor(log_file=BAR_filename) as barometer,
+          Gnss(log_file=GNSS_filename) as gnss,
           EspBridge() as esp_bridge):
         starttime = time_ns()
         while keep_running:
-            keep_running = control_loop(tof, bar, gps, esp_bridge)
+            keep_running = control_loop(tof, barometer, gnss, esp_bridge)
             time.sleep(0.018)
