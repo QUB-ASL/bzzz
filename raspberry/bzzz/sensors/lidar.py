4a3e25c8ffb032dab934c7864f3db3b35cefb35c
Ethan <ejb@ejb-dell.localdomain>
2024-09-09 00:04:53 +0100

    Fix driver so it scans properly and doesn't have threadlocking issues. Locking is unnecessary due to the FIFO nature of the scanlist buffer.

--- raspberry/bzzz/sensors/lidar.py
+++ raspberry/bzzz/sensors/lidar.py
@@ -1,9 +1,11 @@
 from threading import Thread, Lock
-from .data_logger import DataLogger
+from data_logger import DataLogger
 from rplidar import RPLidar
 import time
 import datetime
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as ani
 
 class lidar:
     """
@@ -29,28 +31,29 @@ class lidar:
                             or "express"; default: "normal"
         :param log_file: File name to log data; default: None
         :param max_samples: maximum number of samples to record; default:
-                            100000
+            100000
         :param scan_size: number of measurements per scan; default: 100
         :param window_length: number of scans to be stored; default: 3
 
         If `log_file` is None, the data is not logged; otherwise, on exit,
         the data are stored in a CSV file
         """
-        
+
         self.__serial_path = serial_path
         self.__baud = baud
         self.__window_length = window_length
         self.__scan_size = scan_size
+        self.__scan_type = scan_type
         # There are three values per measurement: Quality, Angle, and Range
         self.__values_cache = np.tile(np.nan, (self.__window_length, self.__scan_size, 3))
         self.__cursor = 0
         self.__log_file = log_file
         self.__max_samples = max_samples
         self.__driver = RPLidar(port=self.__serial_path,
-                              baudrate=self.__baud)
-        self.__lock = Lock()
-        self.__thread = Thread(target=self.__get_scans_in_background,
-                               args=[scan_type,3000])
+                                baudrate=self.__baud)
+        self.__iterator = None
+        #self.__lock = Lock()
+        self.__thread = Thread(target=self.__get_scans_in_background,args=[self.__scan_type,self.__scan_size])
         self.__keep_going = True
 
         if log_file is not None:
@@ -59,8 +62,12 @@ class lidar:
                                        max_samples=max_samples,
                                        feature_names=feature_names)
         self.__thread.start()
-    
+        #self.__iterator = self.__driver.iter_scans(self.__scan_type, self.__scan_size, min_len=20)
+    def __get_scans_in_background(self,scan_type,max_buf_meas):
+        self.__iterator = self.__driver.iter_scans(scan_type, max_buf_meas, min_len=10)
 
+
+    '''
     def __get_scans_in_background(self,scan_type,max_buf_meas):
         """
         Continuously reads incoming LiDAR data from the USB serial connection.
@@ -68,19 +75,21 @@ class lidar:
         """
         iterator = self.__driver.iter_scans(scan_type, max_buf_meas, min_len=20)
         while self.__keep_going:
+            time.sleep(1)# Gives "attempting to use port that is not open" error
             with self.__lock:
+                self.__cursor = ((self.__cursor + 1) % self.__window_length)
                 current_scan = next(iterator)
                 self.__values_cache[self.__cursor, 0:len(current_scan), :] = current_scan
                 if (self.__log_file is not None 
-                        and self.__cursor < self.__max_samples):
+                    and self.__cursor < self.__max_samples):
                     current_timestamp = datetime.datetime.now()
                     for measurement in current_scan:
                         self.__logger.record(current_timestamp, measurement)
-                
+
                 self.__cursor = ((self.__cursor + 1) % self.__window_length)
             if not self.__keep_going:
                 return
-
+    '''
 
     def __enter__(self):
         return self
@@ -90,7 +99,7 @@ class lidar:
         self.__keep_going = False
         self.__driver.stop()
         self.__driver.disconnect()
-        self.__thread.join()
+        #self.__thread.join()
         if self.__log_file is not None:
             self.__logger.save_to_csv(self.__log_file)
 
@@ -100,13 +109,14 @@ class lidar:
         """
         Returns latest LiDAR scan in the __values_cache buffer.
         """
-        with self.__lock:# this fails to get the lock for some reason
-            return self.__values_cache[((self.__cursor) % self.__window_length), :, :]
+        return next(self.__iterator)
+        #with self.__lock:# this fails to get the lock for some reason
+        #    return self.__values_cache[((self.__cursor) % self.__window_length), :, :]
+
 
 if __name__ == "__main__":
-    while True:
-        filename = datetime.datetime.now().strftime("Lidar_%d-%m-%y--%H-%M.csv")
-        with lidar(scan_type="express") as lidar_obj:
-            print(lidar_obj.scan[:,2])
-        time.sleep(0.5)
-
+    filename = datetime.datetime.now().strftime("Lidar_%d-%m-%y--%H-%M.csv")
+    with lidar(scan_type="express", log_file=filename) as lidar_obj:
+        while True:
+            scan = lidar_obj.scan
+            print(scan)
