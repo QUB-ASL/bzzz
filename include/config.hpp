2daae83fa490dda05af69e47f64b34006e86dc58
Manoj Ravi <manojmanny17@gmail.com>
2026-06-16 22:19:45 +0100

    Update ESC pin definitions for hexacopter configuration

--- include/config.hpp
+++ include/config.hpp
@@ -50,8 +50,14 @@
 #define BACK_RIGHT_ESC_PIN 26
 
 #if UAV_TYPE == UAV_TYPE_HEXACOPTER
-#define MIDDLE_LEFT_ESC_PIN 12   // port 3 
-#define MIDDLE_RIGHT_ESC_PIN 14  // port 3
+#define FRONT_LEFT_ESC_PIN 33
+#define FRONT_RIGHT_ESC_PIN 14
+
+#define MID_LEFT_ESC_PIN 25
+#define MID_RIGHT_ESC_PIN 26
+
+#define BACK_LEFT_ESC_PIN 12
+#define BACK_RIGHT_ESC_PIN 27
 #endif
 
 
